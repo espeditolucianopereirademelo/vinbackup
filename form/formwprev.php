474512899b00de85048b17b3c80200bc93bcfe19
lucianop <lucianop@lpdesktop>
2017-09-08 14:40:12 -0300

    form de contato

--- form/formwprev.php
+++ form/formwprev.php
@@ -1,76 +1,46 @@
 <?php
-//Variáveis
-
-$nome = $_POST['name'];
-$email = $_POST['email'];
-$telefone = $_POST['phone'];
-// $opcoes = $_POST['escolhas'];
-$mensagem = $_POST['mensagem'];
-$data_envio = date('d/m/Y');
-$hora_envio = date('H:i:s');
-
-// Compo E-mail
-  $arquivo = "
-  <style type='text/css'>
-  body {
-  margin:0px;
-  font-family:Verdane;
-  font-size:12px;
-  color: #666666;
-  }
-  a{
-  color: #666666;
-  text-decoration: none;
-  }
-  a:hover {
-  color: #FF0000;
-  text-decoration: none;
-  }
-  </style>
-    <html>
-        <table width='510' border='1' cellpadding='1' cellspacing='1' bgcolor='#CCCCCC'>
-            <tr>
-              <td>
-  <tr>
-                 <td width='500'>Nome:$nome</td>
-                </tr>
-                <tr>
-                  <td width='320'>E-mail:<b>$email</b></td>
-     </tr>
-      <tr>
-                  <td width='320'>Telefone:<b>$telefone</b></td>
-                </tr>
-                <tr>
-                  <td width='320'>Mensagem:$mensagem</td>
-                </tr>
-            </td>
-          </tr>  
-          <tr>
-            <td>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></td>
-          </tr>
-        </table>
-    </html>
-  ";
-  
-  //enviar
-  
-  // emails para quem será enviado o formulário
-  $emailenviar = "contato@vinbackup.com";
-  $destino = $emailenviar;
-  $assunto = "Contato pelo Site";
-
-  // É necessário indicar que o formato do e-mail é html
-  $headers  = 'MIME-Version: 1.0' . "\r\n";
-      $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-      $headers .= 'From: $nome <$email>';
-  //$headers .= "Bcc: $EmailPadrao\r\n";
-  
-  $enviaremail = mail($destino, $assunto, $arquivo, $headers);
-  if($enviaremail){
-  $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
-  echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
-  } else {
-  $mgm = "ERRO AO ENVIAR E-MAIL!";
-  echo "";
-  }
+if (isset($_POST['BTEnvia'])) {
+	
+	//Variaveis de POST, Alterar somente se necessário 
+	//====================================================
+	$nome = $_POST['name'];
+	$email = $_POST['email'];
+	$telefone = $_POST['phone']; 
+	$mensagem = $_POST['mensagem'];
+	//====================================================
+	
+	//REMETENTE --> ESTE EMAIL TEM QUE SER VALIDO DO DOMINIO
+	//==================================================== 
+	$email_remetente = "contato@vinbackup.com"; // deve ser uma conta de email do seu dominio 
+	//====================================================
+	
+	//Configurações do email, ajustar conforme necessidade
+	//==================================================== 
+	$email_destinatario = "contato@vinbackup.com"; // pode ser qualquer email que receberá as mensagens
+	$email_reply = "$email"; 
+	$email_assunto = "Contato pelo site vinbackup.com"; // Este será o assunto da mensagem
+	//====================================================
+	
+	//Monta o Corpo da Mensagem
+	//====================================================
+	$email_conteudo = "Nome = $nome \n"; 
+	$email_conteudo .= "Email = $email \n";
+	$email_conteudo .= "Telefone = $telefone \n"; 
+	$email_conteudo .= "Mensagem = $mensagem \n"; 
+	//====================================================
+	
+	//Seta os Headers (Alterar somente caso necessario) 
+	//==================================================== 
+	$email_headers = implode ( "\n",array ( "From: $email_remetente", "Reply-To: $email_reply", "Return-Path: $email_remetente","MIME-Version: 1.0","X-Priority: 3","Content-Type: text/html; charset=UTF-8" ) );
+	//====================================================
+	
+	//Enviando o email 
+	//==================================================== 
+	if (mail ($email_destinatario, $email_assunto, nl2br($email_conteudo), $email_headers)){ 
+					echo "</b>E-Mail enviado com sucesso!</b>"; 
+					} 
+			else{ 
+					echo "</b>Falha no envio do E-Mail!</b>"; } 
+	//====================================================
+} 
 ?>
